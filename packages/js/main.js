dc8df1d3325fd267e58b425cda2c8accdf627bce
sanajitjana <dev.sanajitjana@gmail.com>
2022-10-23 15:46:24 +0530

    remove unused code

--- packages/js/main.js
+++ packages/js/main.js
@@ -4,7 +4,6 @@ const navMenu = document.getElementById("nav-menu"),
   navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
@@ -12,7 +11,6 @@ if (navToggle) {
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
@@ -24,29 +22,9 @@ const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
   const navMenu = document.getElementById("nav-menu");
-  // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
-
-/*==================== ACCORDION SKILLS ====================*/
-const skillsContent = document.getElementsByClassName("skills__content"),
-  skillsHeader = document.querySelectorAll(".skills__header");
-
-function toggleSkills() {
-  let itemClass = this.parentNode.className;
-
-  for (i = 0; i < skillsContent.length; i++) {
-    skillsContent[i].className = "skills__content skills__close";
-  }
-  if (itemClass === "skills__content skills__close") {
-    this.parentNode.className = "skills__content skills__open";
-  }
-}
-
-skillsHeader.forEach((el) => {
-  el.addEventListener("click", toggleSkills);
-});
 
 /*==================== QUALIFICATION TABS ====================*/
 const tabs = document.querySelectorAll("[data-target]"),
@@ -95,7 +73,6 @@ window.addEventListener("scroll", scrollActive);
 /*==================== CHANGE BACKGROUND HEADER ====================*/
 function scrollHeader() {
   const nav = document.getElementById("header");
-  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
   if (this.scrollY >= 80) nav.classList.add("scroll-header");
   else nav.classList.remove("scroll-header");
 }
@@ -104,14 +81,12 @@ window.addEventListener("scroll", scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
 function scrollUp() {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
   if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
   else scrollUp.classList.remove("show-scroll");
 }
 window.addEventListener("scroll", scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/
-
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "uil-sun";
@@ -128,7 +103,6 @@ const getCurrentIcon = () =>
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme
   );
@@ -139,21 +113,19 @@ if (selectedTheme) {
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener("click", () => {
-  // Add or remove the dark / icon theme
   document.body.classList.toggle(darkTheme);
   themeButton.classList.toggle(iconTheme);
-  // We save the theme and the current icon that the user chose
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-//preloader
+/*==================== PRELOADER ====================*/
 let preloader = document.getElementById("loading");
 function preLoader() {
   preloader.style.display = "none";
 }
 
-//conatct form
+/*==================== FORM SUBMIT ====================*/
 document.getElementById("contact-form").addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -181,21 +153,16 @@ document.getElementById("contact-form").addEventListener("submit", (e) => {
         document.getElementById("email").value = "";
         document.getElementById("message").value = "";
 
-        //button animation
         document.getElementById("email-submit").innerHTML = `
           Send message
           <i class="uil uil-message button__icon"></i>`;
 
-        //success alert
         alert("Your message send susscessfully!");
       })
       .catch((err) => {
-        //button animation
         document.getElementById("email-submit").innerHTML = `
           Send message
           <i class="uil uil-message button__icon"></i>`;
-
-        //console alert
         console.log(err);
       });
   } else {
@@ -203,7 +170,7 @@ document.getElementById("contact-form").addEventListener("submit", (e) => {
   }
 });
 
-// github calender
+/*==================== GITHUB CALENDAR ====================*/
 GitHubCalendar(".calendar", "sanajitjana", {
   responsive: true,
   global_stats: false,
